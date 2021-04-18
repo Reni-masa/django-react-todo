a749c851411ab55f2f9041a1fa7dd863cc13bdb1
SugitaMasataka <mstk.ambit@gmail.com>
2021-04-18 18:33:21 +0900

    [add]api endpint作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .serializers import TaskSerializer
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def apiOverview(request):
+    return JsonResponse("api base point", safe=False)
